93ac3028a1031cfe712e2ba8d560bdf308a73602
André Peil Müller <andrepeilmuller@gmail.com>
2021-01-20 14:02:23 -0300

    ajustando para fazer teste de build

--- frontend/src/service/config.js
+++ frontend/src/service/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: 'https://quiosquemassagem.rj.r.appspot.com/api/',
   headers: {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkVXN1YXJpbyI6MSwidXN1YXJpbyI6ImFkbWluIn0sImlhdCI6MTYxMDAyNjI4OSwiZXhwIjoxOTcwMDIyNjg5fQ.d9Y1p3z8x5NAQiY6_4lyR6H0Ayu72Kp6udRyY2RQedk'
   }
